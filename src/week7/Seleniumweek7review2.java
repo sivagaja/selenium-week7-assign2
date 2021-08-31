525bbe3af7c4b8ba05274a2d58a5a2519c78e763
satish.wingherry <satish.wingherry@gmail.com>
2021-08-31 16:42:36 +0530

    selenium

---
+++ src/week7/Seleniumweek7review2.java
@@ -0,0 +1,22 @@
+package week7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Seleniumweek7review2 {
+	
+      @Test
+	public void test(){
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\tools\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Welcome  to Ebay...");
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+		driver.close();
+		
+	}
+	
+
+}
